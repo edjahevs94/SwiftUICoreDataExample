3d84a4e315f85e0779d2b5bf9a89dc5b4913368e
Edgard <20133252@ealoe.ulima.edu.pe>
2022-09-15 11:30:16 -0500

    Initial Commit

---
+++ CoreDataDemo/CoreDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDemoApp.swift
+//  CoreDataDemo
+//
+//  Created by EdgardVS on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
